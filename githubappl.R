6dbc71207ef12d8bc9c70111e7786e6b6cb2b922
juhaszle <juhaszle@freemail.hu>
2014-04-19 20:58:45 +0200

    4th commit

--- githubappl.R
+++ githubappl.R
@@ -1,3 +1,14 @@
-getwd()
+library(httr)
+library(httpuv)
+oauth_endpoints("github")
+myapp <- oauth_app("github", "bcc856e74de8c13b2751", "a15744ae19217df77374f1153bc45143b5939c1b")
+github_token <- oauth2.0_token(oauth_endpoints("github"), myapp)
+req <- GET("https://api.github.com/users/jtleek/repos", config(token = github_token))
+stop_for_status(req)
+json1=content(req)
 
-
+for (i in 1:length(json1)){
+  if (json1[[i]]$name=='datasharing'){
+    print (json1[[i]]$created_at)
+  }
+}
